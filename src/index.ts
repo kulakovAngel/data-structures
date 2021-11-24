2940298084934c9c197afd21d4e692a23e634ead
ValeryKulakov <kulakov.resurs@tut.by>
2021-11-24 20:59:50 +0300

    Add namespaces

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import {LinkedList} from './lists';
+import List = LinkedList.List;
 
 class App {
     public static main(): void {
-        const nList: LinkedList<number> = new LinkedList();
+        const nList: LinkedList.List<number> = new LinkedList.List();
         nList.push(15);
         nList.push(96);
         nList.push(-8);
         console.log(nList);
 
-        const strList: LinkedList<string> = new LinkedList();
+        const strList: List<string> = new List();
         strList.push('Hello');
         strList.push('world');
-        console.log(strList)
+        console.log(strList);
     };
 }
 
--- src/lists.ts
+++ src/lists.ts
@@ -1,47 +1,46 @@
-// todo: add namaspaces
+export namespace LinkedList {
+    type TNode<T> = INode<T> | null;
 
-type TNode<T> = INode<T> | null;
-
-interface INode<T> {
-    setNext(next: INode<T>): void;
-    getNext(): TNode<T>;
-    getData(): T;
-}
-
-class CNode<T> implements INode<T> {
-    private next: TNode<T> = null;
-
-    constructor(private data: T) {};
-
-    public setNext(next: INode<T>) {
-        this.next = next;
-    };
-
-    public getNext(): TNode<T> {
-        return this.next;
-    };
-
-    public getData(): T {
-        return this.data;
-    };
-}
-
-export class LinkedList<T> {
-    private root: TNode<T> = null;
-    private last: TNode<T> = null;
-
-    constructor() {};
-    private createRoot(data: T) {
-        this.root = new CNode(data);
-        this.last = this.root;
+    interface INode<T> {
+        setNext(next: INode<T>): void;
+        getNext(): TNode<T>;
+        getData(): T;
     }
-    public push(data: T) {
-        if (!this.last) {
-            this.createRoot(data);
-        } else {
-            const newNode = new CNode(data)
-            this.last.setNext(newNode);
-            this.last = newNode;
+    
+    class Node<T> implements INode<T> {
+        private next: TNode<T> = null;
+    
+        constructor(private data: T) {};
+    
+        public setNext(next: INode<T>) {
+            this.next = next;
+        };
+    
+        public getNext(): TNode<T> {
+            return this.next;
+        };
+    
+        public getData(): T {
+            return this.data;
+        };
+    }
+    
+    export class List<T> {
+        private root: TNode<T> = null;
+        private last: TNode<T> = null;
+
+        private createRoot(data: T) {
+            this.root = new Node(data);
+            this.last = this.root;
         }
-    };
+        public push(data: T) {
+            if (!this.last) {
+                this.createRoot(data);
+            } else {
+                const newNode = new Node(data)
+                this.last.setNext(newNode);
+                this.last = newNode;
+            }
+        };
+    }
 }
