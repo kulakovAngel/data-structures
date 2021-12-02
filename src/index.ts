75aec36a5e8282060a894c45595762bfa11593c1
ValeryKulakov <kulakov.resurs@tut.by>
2021-12-02 23:05:35 +0300

    Add Iterator<T>

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,32 @@ class App {
         nList.push(15);
         nList.push(96);
         nList.push(-8);
-        // console.log(nList);
-        nList.view();
+
+        const nListIterator = nList.getIterator();
+
+        this.printListToConsole(nListIterator);
+
         nList.add(0, 96);
         nList.add(1000, 0);
         nList.delete(0);
         console.log('=====================');
-        nList.view();
 
+        this.printListToConsole(nListIterator);
+        
         const strList: List<string> = new List();
         strList.push('Hello');
         strList.push('world');
         console.log(strList);
     };
+
+    public static printListToConsole(nListIterator: LinkedList.IIterator<number>) {
+        nListIterator.rewind();
+        while(!nListIterator.isDone()) {
+            const curr = nListIterator.current();
+            nListIterator.next();
+            console.log(curr);
+        }
+    }
 }
 
 App.main();
--- src/lists.ts
+++ src/lists.ts
@@ -1,22 +1,20 @@
 export namespace LinkedList {
-    type TNode<T> = INode<T> | null;
-
     interface INode<T> {
-        setNext(next: TNode<T>): void;
-        getNext(): TNode<T>;
+        setNext(next: INode<T>): void;
+        getNext(): INode<T>;
         getData(): T;
     }
     
     class Node<T> implements INode<T> {
-        private next: TNode<T> = null;
+        private next: INode<T>;
     
         constructor(private data: T) {};
     
-        public setNext(next: TNode<T>) {
+        public setNext(next: INode<T>) {
             this.next = next;
         };
     
-        public getNext(): TNode<T> {
+        public getNext(): INode<T> {
             return this.next;
         };
     
@@ -26,13 +24,14 @@ export namespace LinkedList {
     }
     
     export class List<T> {
-        private root: TNode<T> = null;
-        private last: TNode<T> = null;
+        private root: INode<T>;
+        private last: INode<T>;
 
         private createRoot(data: T) {
             this.root = new Node(data);
             this.last = this.root;
         }
+
         public push(data: T) {
             if (!this.last) {
                 this.createRoot(data);
@@ -43,23 +42,13 @@ export namespace LinkedList {
             }
         };
 
-        // TODO: set the "viewer callback";
-        public view() {
-            let current: TNode<T> = this.root;
-            while (current) {
-                const data: T = current.getData();
-                console.log('Current node data: ', data)
-                current = current.getNext();
-            }
-        }
-
         public add(data: T, afterNode: T) {
-            let current: TNode<T> = this.root;
+            let current: INode<T> = this.root;
             while (current) {
                 const currentData: T = current.getData();
 
                 if (afterNode === currentData) {
-                    const newNode: TNode<T> = new Node<T>(data);
+                    const newNode: INode<T> = new Node<T>(data);
                     const next = current.getNext();
 
                     newNode.setNext(next);
@@ -72,9 +61,8 @@ export namespace LinkedList {
             }
         }
 
-        // TODO: unify with add and view;
         public delete(dataToDelete: T) {
-            let current: TNode<T> = this.root;
+            let current: INode<T> = this.root;
             while (current) {
                 const next = current.getNext();
 
@@ -89,5 +77,44 @@ export namespace LinkedList {
                 current = current.getNext();
             }
         }
+
+        public getIterator(): IIterator<T> {
+            return new Iterator<T>(this.root);
+        }
+    }
+
+    export interface IIterator<T> {
+        current(): T;
+        next(): void;
+        isDone(): boolean;
+        rewind(): void;
+    }
+
+    class Iterator<T> implements IIterator<T> {
+        private currentNode: INode<T>;
+        private root: INode<T>;
+
+        constructor(collection: INode<T>) {
+            this.currentNode = collection;
+            this.root = collection;
+        };
+
+        public current(): T {
+            const data = this.currentNode.getData();
+            return data;
+        };
+
+        public next(): void {
+            this.currentNode = this.currentNode.getNext();
+        };
+
+        public isDone() {
+            const isNextExist = this.currentNode;
+            return !isNextExist;
+        };
+
+        public rewind() {
+            this.currentNode = this.root;
+        };
     }
 }
